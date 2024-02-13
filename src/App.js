5e046d0fa8780d5db50ea474c6b126e04e1c061a
abhilashdurgam454 <durgamabhilash15@gmail.com>
2024-02-12 22:15:05 -0800

    first

--- src/App.js
+++ src/App.js
@@ -4,8 +4,22 @@ import './style.css';
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <nav className='box'>
+        <div className='logo'>
+            <img src="/images/brand_logo.png" alt="logo" />
+        </div>
+      
+      <ul className='header'>
+
+       <li href="#">HOME</li>
+       <li href="#">LOCATION</li>
+       <li href="#">ABOUT</li>
+       <li href="#">CONTACT</li>
+
+      </ul>
+       
+       <button className=' right'>login</button>
+    </nav>
     </div>
   );
 }
